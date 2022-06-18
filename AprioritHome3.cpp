579755948e11987afcf400a859cb4c2e69032a62
Greshch <greshchook@gmail.com>
2022-06-18 14:56:10 +0300

    simple was implemented

--- AprioritHome3.cpp
+++ AprioritHome3.cpp
@@ -3,8 +3,49 @@
 
 #include <iostream>
 
+//Enter the first num : 10
+//Enter the operation : +
+//Enter the second num : 20
+//Result is : 30
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Enter the first num : ";
+    double first = 0;
+    std::cin >> first;
+
+    std::cout << "Enter the operation : ";
+    char operation = 0;
+    std::cin >> operation;
+
+    std::cout << "Enter the second num : ";
+    double second = 0;
+    std::cin >> second;
+
+    double result = .0;
+
+    if (operation == '*')
+    {
+        result = first * second;
+    }
+    else if (operation == '/')
+    {
+        result = first / second;
+    }
+    else if (operation == '+')
+    {
+        result = first + second;
+    }
+    else if (operation == '-')
+    {
+        result = first - second;
+    }
+    else
+    {
+        std::cout << "Wrong nput...ERROR\n";
+        return 404;
+    }
+
+    std::cout << "Result is : " << result << std::endl;
 }
 
